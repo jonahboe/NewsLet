6240630fbe0a0b05c979a9fe42ad16186b66656d
jonahboe <jonahwb@byui.edu>
2020-06-15 00:31:15 -0600

    Added some backend tools to make other pages avalible

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'Express'
-  });
-});
+const indexController = require('../controllers/index');
+
+
+router.get('/', indexController.getNews);
+router.get('/saved-articles', indexController.getSavedArticles);
+router.get('/my-posts', indexController.getMyPosts);
+router.post('/search', indexController.postSearch);
+router.post('/login', indexController.postLogin);
+router.post('/logout', indexController.postLogout);
+router.post('/search', indexController.postSearch);
+
 
 module.exports = router;
